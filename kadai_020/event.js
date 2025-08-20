2f93cc88d486abde00bd3ec75619d50dc7da11ce
ObaraTakumi <59912124+takumi02r@users.noreply.github.com>
2025-08-20 19:47:54 +0900

    kadai_020の追加

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+  
+  const text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+});
